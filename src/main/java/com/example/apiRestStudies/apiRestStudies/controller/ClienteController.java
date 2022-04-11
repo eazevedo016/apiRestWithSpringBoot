d62b0262f52aedabffed2c0bcd785803aca487f2
eazevedo.016@gmail.com <eduardoazevedo@sisconsultoria.com.br>
2022-04-11 10:55:18 -0400

    finalizando controller

--- src/main/java/com/example/apiRestStudies/apiRestStudies/controller/ClienteController.java
+++ src/main/java/com/example/apiRestStudies/apiRestStudies/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.example.apiRestStudies.apiRestStudies.controller;
 
+
 import com.example.apiRestStudies.apiRestStudies.model.Cliente;
 import com.example.apiRestStudies.apiRestStudies.repository.ClienteRepository;
 
@@ -7,8 +8,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+
 
 @RestController
 @RequestMapping("/clientes")
@@ -21,6 +28,12 @@ public class ClienteController {
     @GetMapping
     public List<Cliente> listar() {
         return clienteRepository.findAll();    
+    }
+
+    @PostMapping
+    @ResponseStatus (HttpStatus.CREATED)
+    public Cliente cadastrar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
     }
 
     
